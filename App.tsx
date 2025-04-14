e981170bff5f52c88e4856d692255004d1a78431
yaowen <yaowen@bwton.com>
2025-04-14 21:46:51 +0800

    react基础 https://reactnative.cn/docs/intro-react

--- App.tsx
+++ App.tsx
@@ -5,19 +5,25 @@
  * @format
  */
 
-import React from 'react';
-import {Text, View} from 'react-native';
+import React from "react";
+import { Image, ScrollView, Text, View } from "react-native";
 
 function App(): JSX.Element {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Try editing me!</Text>
-    </View>
+    <ScrollView>
+      <Text>Some Text</Text>
+      <View>
+        <Text>Some more text</Text>
+        <Image
+          source={{ uri: 'h"https://reactnative.dev/docs/assets/p_cat2.png"}
+          style={{ width: 200, height: 200 }}
+        />
+      </View>
+      <TextInput
+        style={{ height: 40, borderColor: 'g"gray"borderWidth: 1 }}
+        defaultValue="you can type in me"
+      />
+    </ScrollView>
   );
 }
 
